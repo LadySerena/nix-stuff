1fe59f7a76eabafc1d1892f84635bc55a4afffcd
LadySerena <serena.tiede+github@gmail.com>
2024-03-24 15:35:37 -0500

    refact: new terminal theme

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require 'wezterm'
 
 return {
-  color_scheme = 'Nancy (terminal.sexy)',
+  color_scheme = 'Neon',
   font = wezterm.font_with_fallback { 'Comic Code Ligatures', 'FantasqueSansM Nerd Font' },
   font_size = 14,
 }
