3ba8cc3468e19304e0ddc145d41d19a993fd0de7
LadySerena <serena.tiede+github@gmail.com>
2024-04-19 22:37:54 -0500

    refactor: put common files into common dir

---
+++ common/terminal.nix
@@ -0,0 +1,40 @@
+{
+
+  programs.zsh = {
+    enable = true;
+    autosuggestion = {
+      enable = true;
+    };
+
+    initExtra = ''
+      . "$HOME/.cargo/env"
+      bindkey "^[," backward-word
+      bindkey "^[." forward-word
+      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+      fi
+      setopt interactivecomments
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      command_timeout = 1500;
+      directory = {
+        truncate_to_repo = false;
+      };
+      username = {
+        show_always = true;
+      };
+      kubernetes = {
+        disabled = false;
+      };
+    };
+  };
+}
